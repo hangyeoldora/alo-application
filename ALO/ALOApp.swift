b5c34a3d53035119b5ebea0df673c3453990e1b4
hangyeoldora <hg14699@naver.com>
2021-06-29 11:13:40 +0900

    Initial Commit

---
+++ ALO/ALOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALOApp.swift
+//  ALO
+//
+//  Created by 이한결 on 2021/06/29.
+//
+
+import SwiftUI
+
+@main
+struct ALOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ALO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ALO
+//
+//  Created by 이한결 on 2021/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
